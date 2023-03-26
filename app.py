3d50871a6fc4b71987f5d858b96476c827e9fb2a
amit <amitsahoo1989@gmail.com>
2023-03-26 10:01:54 +0530

    added html and dynamic data

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+defined_jobs = [
+    {
+        "id":1,
+        "title":"Data Science",
+        "location": "Hyderabad",
+        "Salary": 1000.00,
+        "Work Type": "Remote"
+    },
+    {
+            "id":2,
+            "title":"Data Analyst",
+            "location": "Bengaluru",
+            "Salary": 900.00
+    },
+    {
+            "id":3,
+            "title":"Frontend Developer",
+            "location": "Bhubaneswar",
+            "Salary": 1100.00,
+            "Work Type": "Hybrid"
+    },
+    {
+            "id":4,
+            "title":"Data Engineer",
+            "location": "Hyderabad",
+            "Salary": 1500.00,
+            "Work Type": "Office"
+    }
 
+]
 @app.route("/")
 def hello_world():
-    return "Hello World"
+    return render_template("home.html", jobs=defined_jobs, company_name = "Amit")
 
 
 if __name__ == "__main__":
