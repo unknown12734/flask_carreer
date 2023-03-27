d06ed06f96e2ba8d6b4f38d6f65fb1448434635f
amit <amitsahoo1989@gmail.com>
2023-03-28 00:33:33 +0530

    added the second page

--- app.py
+++ app.py
@@ -1,40 +1,24 @@
 from flask import Flask, render_template
+from data import defined_jobs
+from flask import jsonify
 
 app = Flask(__name__)
 
-defined_jobs = [
-    {
-        "id":1,
-        "title":"Data Science",
-        "location": "Hyderabad",
-        "Salary": 1000.00,
-        "Work Type": "Remote"
-    },
-    {
-            "id":2,
-            "title":"Data Analyst",
-            "location": "Bengaluru",
-            "Salary": 900.00
-    },
-    {
-            "id":3,
-            "title":"Frontend Developer",
-            "location": "Bhubaneswar",
-            "Salary": 1100.00,
-            "Work Type": "Hybrid"
-    },
-    {
-            "id":4,
-            "title":"Data Engineer",
-            "location": "Hyderabad",
-            "Salary": 1500.00,
-            "Work Type": "Office"
-    }
-
-]
 @app.route("/")
 def hello_world():
     return render_template("home.html", jobs=defined_jobs, company_name = "Amit")
+
+@app.route("/app/data")
+def hello_world_api_data():
+    return jsonify(results = defined_jobs)
+
+
+@app.route("/job/<id>")
+def specific_job_api_data(id):
+    result = list(filter(lambda x: (x["id"]==int(id)), defined_jobs))
+    if not result:
+        return "Not Found", 404
+    return render_template("job.html", result=result, company_name = "Amit")
 
 
 if __name__ == "__main__":
---
+++ data.py
@@ -0,0 +1,30 @@
+defined_jobs = [
+    {
+        "id":1,
+        "title":"Data Science",
+        "location": "Hyderabad",
+        "Salary": 1000.00,
+        "Work Type": "Remote"
+    },
+    {
+            "id":2,
+            "title":"Data Analyst",
+            "location": "Bengaluru",
+            "Salary": 900.00
+    },
+    {
+            "id":3,
+            "title":"Frontend Developer",
+            "location": "Bhubaneswar",
+            "Salary": 1100.00,
+            "Work Type": "Hybrid"
+    },
+    {
+            "id":4,
+            "title":"Data Engineer",
+            "location": "Hyderabad",
+            "Salary": 1500.00,
+            "Work Type": "Office"
+    }
+
+]
