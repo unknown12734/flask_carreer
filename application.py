9b324141e2f645fd7b10dd41b29d4edc5eafacd5
amit <amitsahoo1989@gmail.com>
2023-03-28 01:06:33 +0530

    modified requirements.txt

--- application.py
+++ application.py
@@ -2,18 +2,18 @@ from flask import Flask, render_template
 from data import defined_jobs
 from flask import jsonify
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def hello_world():
     return render_template("home.html", jobs=defined_jobs, company_name = "Amit")
 
-@app.route("/app/data")
+@application.route("/app/data")
 def hello_world_api_data():
     return jsonify(results = defined_jobs)
 
 
-@app.route("/job/<id>")
+@application.route("/job/<id>")
 def specific_job_api_data(id):
     result = list(filter(lambda x: (x["id"]==int(id)), defined_jobs))
     if not result:
@@ -22,4 +22,4 @@ def specific_job_api_data(id):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    application.run(host="0.0.0.0", debug=True)
