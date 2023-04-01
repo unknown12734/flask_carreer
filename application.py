e4b04b13e451a7825e1e6899942d9309b6e65f93
amit <amitsahoo1989@gmail.com>
2023-04-01 21:16:42 +0530

    added database

--- application.py
+++ application.py
@@ -1,21 +1,30 @@
 from flask import Flask, render_template
-from data import defined_jobs
+from data import session, Job
 from flask import jsonify
 
 application = Flask(__name__)
 
+jobs = session.query(Job)
+
+def load_data(session,jobs):
+    defined_jobs = []
+    for job in jobs:
+        defined_jobs.append(job.__dict__)
+    return defined_jobs
+
+
 @application.route("/")
 def hello_world():
-    return render_template("home.html", jobs=defined_jobs, company_name = "Amit")
+    return render_template("home.html", jobs=load_data(session,jobs), company_name = "Priyanka")
 
 @application.route("/app/data")
 def hello_world_api_data():
-    return jsonify(results = defined_jobs)
+    return jsonify(results = load_data(session,jobs))
 
 
 @application.route("/job/<id>")
 def specific_job_api_data(id):
-    result = list(filter(lambda x: (x["id"]==int(id)), defined_jobs))
+    result = list(filter(lambda x: (x["id"]==int(id)), load_data(session,jobs)))
     if not result:
         return "Not Found", 404
     return render_template("job.html", result=result, company_name = "Amit")
--- data.py
+++ data.py
@@ -1,30 +1,62 @@
-defined_jobs = [
-    {
-        "id":1,
-        "title":"Data Science",
-        "location": "Hyderabad",
-        "Salary": 1000.00,
-        "Work Type": "Remote"
-    },
-    {
-            "id":2,
-            "title":"Data Analyst",
-            "location": "Bengaluru",
-            "Salary": 900.00
-    },
-    {
-            "id":3,
-            "title":"Frontend Developer",
-            "location": "Bhubaneswar",
-            "Salary": 1100.00,
-            "Work Type": "Hybrid"
-    },
-    {
-            "id":4,
-            "title":"Data Engineer",
-            "location": "Hyderabad",
-            "Salary": 1500.00,
-            "Work Type": "Office"
-    }
+# defined_jobs = [
+#     {
+#         "id":1,
+#         "title":"Data Science",
+#         "location": "Hyderabad",
+#         "Salary": 1000.00,
+#         "Work Type": "Remote"
+#     },
+#     {
+#             "id":2,
+#             "title":"Data Analyst",
+#             "location": "Bengaluru",
+#             "Salary": 900.00
+#     },
+#     {
+#             "id":3,
+#             "title":"Frontend Developer",
+#             "location": "Bhubaneswar",
+#             "Salary": 1100.00,
+#             "Work Type": "Hybrid"
+#     },
+#     {
+#             "id":4,
+#             "title":"Data Engineer",
+#             "location": "Hyderabad",
+#             "Salary": 1500.00,
+#             "Work Type": "Office"
+#     }]
+from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-]
+username = 'Rinku'
+password = 'Rinku25112021'
+host = 'rinku.cqhvo2hszsw7.us-east-1.rds.amazonaws.com'
+port = 3306
+DB_NAME = 'rinku'
+
+engine = create_engine(f"mysql+pymysql://{username}:{password}@{host}/{DB_NAME}")
+
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+Base = declarative_base()
+
+class Job(Base):
+    __tablename__="job"
+    id = Column(Integer, primary_key=True)
+    title = Column(String(50), nullable=False)
+    location = Column(String(50), nullable=True)
+    Salary = Column(Float, nullable=False)
+    work_Type = Column(String(50), nullable=True)
+
+
+Base.metadata.create_all(engine)
+
+# job1 = Job(title="Frontend Developer",location= "Bhubaneswar",Salary= 1100.00,work_Type= "Hybrid")
+#
+#
+# session.add(job1)
+# session.commit()
